116c1054ba55bee44d9eb79fb15416b4de9e759c
Hiroyuki Hasebe <hiroyha@microsoft.com>
2020-09-14 15:09:15 +0900

    enable cors; add file upload ui

--- src/uploader/src/main/java/com/samples/uploader/controller/UploadController.java
+++ src/uploader/src/main/java/com/samples/uploader/controller/UploadController.java
@@ -9,12 +9,14 @@ import java.util.Date;
 import com.samples.uploader.service.BlobService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @RestController
 @RequestMapping("upload")
 public class UploadController {
